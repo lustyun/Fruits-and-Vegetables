88cd9aeb7028b28accf77b45d91ec5691e824283
Fardus Ahmed <fardus11@gmail.com>
2023-08-18 14:20:25 -0400

    first commit

---
+++ models/Veggies.js
@@ -0,0 +1,65 @@
+    const vegetables =  [
+      {
+        "name": "Carrot",
+        "scientific_name": "Daucus carota",
+        "nutritional_highlights": "Rich in beta-carotene, fiber, and antioxidants",
+        "culinary_uses": ["Salads", "Soups", "Stews", "Snacks"]
+      },
+      {
+        "name": "Broccoli",
+        "scientific_name": "Brassica oleracea var. italica",
+        "nutritional_highlights": "High in vitamin C, vitamin K, fiber, and antioxidants",
+        "culinary_uses": ["Steamed", "Boiled", "Stir-fried", "Roasted", "Casseroles"]
+      },
+      {
+        "name": "Tomato",
+        "scientific_name": "Solanum lycopersicum",
+        "nutritional_highlights": "Good source of vitamin C, potassium, and lycopene",
+        "culinary_uses": ["Salads", "Sandwiches", "Sauces", "Soups", "Cooked dishes"]
+      },
+      {
+        "name": "Cucumber",
+        "scientific_name": "Cucumis sativus",
+        "nutritional_highlights": "Low in calories, high in water content, vitamins, and minerals",
+        "culinary_uses": ["Salads", "Snacks", "Pickles"]
+      },
+      {
+        "name": "Bell Pepper",
+        "scientific_name": "Capsicum annuum",
+        "nutritional_highlights": "Rich in vitamin C, vitamin A, and antioxidants",
+        "culinary_uses": ["Salads", "Stir-fries", "Stuffed dishes", "Garnishes"]
+      },
+      {
+        "name": "Spinach",
+        "scientific_name": "Spinacia oleracea",
+        "nutritional_highlights": "High in iron, vitamin K, vitamin A, and nutrients",
+        "culinary_uses": ["Salads", "Cooked dishes", "Smoothies"]
+      },
+      {
+        "name": "Zucchini",
+        "scientific_name": "Cucurbita pepo",
+        "nutritional_highlights": "Low in calories, vitamins, and minerals",
+        "culinary_uses": ["Grilled", "Sautéed", "Casseroles", "Spiralized"]
+      },
+      {
+        "name": "Eggplant",
+        "scientific_name": "Solanum melongena",
+        "nutritional_highlights": "Contains fiber, antioxidants, and vitamins",
+        "culinary_uses": ["Grilled", "Roasted", "Casseroles", "Meat substitute"]
+      },
+      {
+        "name": "Lettuce",
+        "scientific_name": "Lactuca sativa",
+        "nutritional_highlights": "Low in calories, suitable for salads and wraps",
+        "culinary_uses": ["Salads", "Sandwiches", "Wraps"]
+      },
+      {
+        "name": "Onion",
+        "scientific_name": "Allium cepa",
+        "nutritional_highlights": "Contains antioxidants and potential health benefits",
+        "culinary_uses": ["Flavor base", "Raw in salads", "Cooked in dishes"]
+      }
+    ]
+  
+
+  module.exports = vegetables;
